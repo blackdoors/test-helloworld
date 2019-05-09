d8b30da1c06748ff902862806453ef184d0ca986
Christofer <christofer@sucorinvestam.com>
2019-05-09 14:40:13 +0700

    add line of code

--- GitTest/src/HelloTest.java
+++ GitTest/src/HelloTest.java
@@ -4,6 +4,7 @@ public class HelloTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("setaerae");
+		System.out.println("add line of code");
 	}
 
 }
